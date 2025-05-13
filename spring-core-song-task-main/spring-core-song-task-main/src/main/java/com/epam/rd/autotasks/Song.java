f8c730e928d5c980f03b683def9a8a2d2042f981
ZhenoID <zholdybay.inkar@gmail.com>
2025-05-13 19:04:49 +0500

    Add files via upload

---
+++ spring-core-song-task-main/spring-core-song-task-main/src/main/java/com/epam/rd/autotasks/Song.java
@@ -0,0 +1,45 @@
+package com.epam.rd.autotasks;
+
+public class Song {
+    private String title;
+    private String artist;
+    private int year;
+
+    public Song() { }
+
+    public Song(String title, String artist, int year) {
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+               "title='" + title + '\'' +
+               ", artist='" + artist + '\'' +
+               ", year=" + year +
+               '}';
+    }
+}
---
+++ spring-core-song-task-main/spring-core-song-task-main/src/main/java/com/epam/rd/autotasks/config/SongConfig.java
@@ -0,0 +1,26 @@
+package com.epam.rd.autotasks.config;
+
+import com.epam.rd.autotasks.Song;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class SongConfig {
+
+    @Value("${song.title}")
+    private String title;
+
+    @Value("${song.artist}")
+    private String artist;
+
+    @Value("${song.year}")
+    private int year;
+
+    @Bean
+    public Song song() {
+        return new Song(title, artist, year);
+    }
+}
